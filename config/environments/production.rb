dca03c16292c0aed9754f55a6ef6242bb803b033
atheri <corylotze@gmail.com>
2015-12-10 11:21:30 -0800

    Setup user password recovery via email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # enable mailer so Devise can send email
+  config.action_mailer.default_url_options = { :host => 'http://104.236.172.222/final' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  # Disable delivery errors, bad email addresses will be ignored
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 end
