6d038c84b364579a0b073ce5b7bd7d2aec14fb80
atheri <corylotze@gmail.com>
2015-12-11 22:21:07 -0800

    Added desc column to boards table. Made alerts have a close buton and appear over the navbar instead of displacing it

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20151211193324) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "name"
+    t.string   "desc"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
